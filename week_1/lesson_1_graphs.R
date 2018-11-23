30f30d84eb1c63286e66d9f42b273e195f7e2be4
Jason Bartley <jbartley11@gmail.com>
2018-11-23 13:44:07 -0600

    initial commit

---
+++ week_1/lesson_1_graphs.R
@@ -0,0 +1,26 @@
+# general rules to follow when building graphics
+# book beautiful evidence by Edward Tufte
+
+# 1. show comparisons
+# evidence for a hypothesis is always relative to another competing hypothesis
+# control home vs air cleaner homes
+
+# 2. show causality, mechanism, explanation, systematic structure
+# what's the causal framework
+# why does air cleaner cause a better env
+# particulate matter between control vs air cleaner
+# how world works
+
+# 3. show multivariate data
+# more than 2 variables, real world is multivariate
+# tell richer story
+
+# 4. Integrate evidence
+# integrate words, numbers, images, diagrams
+# don't let tool drive the analysis
+
+# 5. Describe and document the evidence with appropriate labels, scales, sources, etc
+
+# 6. Content is king
+# tell an interesting story
+# be critical of quality, relevance and integrity
